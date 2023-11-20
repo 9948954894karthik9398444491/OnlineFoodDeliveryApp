b89cd0693ed9c83eb2089b961e1146c655d59990
SIRI <siripalakiti2000@gmail.com>
2023-11-21 00:32:26 +0530

    Junit testings JWT token login

---
+++ src/main/java/com/hexaware/fooddelivery/entity/Admin.java
@@ -0,0 +1,65 @@
+package com.hexaware.fooddelivery.entity;
+
+import jakarta.persistence.Entity;
+/*
+ * 
+ * @Author:Karthik
+ * Date:14-11-2023
+ * Description: Entity class of Orders
+ * 
+ * 
+ */
+import jakarta.persistence.Id;
+
+@Entity
+public class Admin {
+	
+	
+	@Id
+	private int adminId;
+    private String userName;
+    private String password;
+    private final String role="ADMIN";
+	public Admin() {
+		super();
+	}
+	public Admin(int adminId, String userName, String password) {
+		super();
+		this.adminId = adminId;
+		this.userName = userName;
+		this.password = password;
+	}
+	public int getAdminId() {
+		return adminId;
+	}
+	public void setAdminId(int adminId) {
+		this.adminId = adminId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	@Override
+	public String toString() {
+		return "Admin [adminId=" + adminId + ", userName=" + userName + ", password=" + password + ", role=" + role
+				+ "]";
+	}
+	
+	
+	
+	
+	
+	
+
+}
